406a531c9e31fbe818c93a9ddc8b99cbfc683916
Your GitHub Username <your.github@email.com>
2025-11-27 02:27:01 +0000

    Fix typo in import: @tanstack/react-query (was @tantml:query)

--- kelmah-frontend/src/main.jsx
+++ kelmah-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { SnackbarProvider } from 'notistack';
-import { QueryClientProvider } from '@tantml:query/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './config/queryClient';
 import store from './store';
